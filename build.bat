338baafb5aa2b4540e1a675709c6fdfd7bfbf8b0
Max T <maxtyuterev@gmail.com>
2021-12-02 10:36:16 -0500

    Reorganize folder structure

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-g++ -Isrc/include -Lsrc/lib -o Snake.exe main.cpp -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -lSDL2_ttf -static-libgcc -static-libstdc++ -mwindows
+g++ -Isrc/include -Lsrc/lib -o bin/Snake.exe main.cpp -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -lSDL2_ttf -static-libgcc -static-libstdc++ -mwindows -O3
